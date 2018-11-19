bed18d6709f8b5b696ff6afc7da070d73c63c88f
Jean-Michel Bruenn <jean.bruenn@ip-minds.de>
2018-11-19 23:12:51 +0100

    corrected aHash

--- finder/aHashBased.class.php
+++ finder/aHashBased.class.php
@@ -5,11 +5,12 @@
  *
  * This class uses an aHash-based-approach to find duplicates
  * by scaling down the photo to 8x8px, getting the average (mean)
- * and thresholding the image. Finally the IM Fingerprint is retrieved
+ * and thresholding the image. Finally the Hash is created
  *
  * @author Jean-Michel Bruenn <himself@jeanbruenn.info>
  * @copyright 2018 <himself@jeanbruenn.info>
  * @license https://opensource.org/licenses/MIT The MIT License
+ * @see https://www.hackerfactor.com/blog/?/archives/432-Looks-Like-It.html
  */
 class aHashBased extends GenericFinder implements Finder
 {
@@ -19,19 +20,29 @@ class aHashBased extends GenericFinder implements Finder
     private $hashes = [];
 
     /**
-     * @param string $file
+     * @param $file
      * @throws ImagickException
+     * @throws ImagickPixelException
      */
     public function searchDuplicates($file)
     {
         $im = new \Imagick($file);
-        $im->blurImage(0, 1);
-        $im->sampleImage(8, 8);
         $im->transformImageColorspace(\Imagick::COLORSPACE_GRAY);
+        $im->sampleImage(8, 8);
         $data = $im->getImageChannelMean(\Imagick::CHANNEL_RED);
         $mean = $data['mean'];
         $im->thresholdImage($mean);
-        $hash = $im->getImageSignature();
+        $bits = "";
+        $imageIterator = $im->getPixelIterator();
+        foreach ($imageIterator as $pixels) {
+            /** @var $pixel \ImagickPixel * */
+            foreach ($pixels as $pixel) {
+                /** @todo 255? */
+                $bits .= $pixel->getcolor()['b'] / 255;
+            }
+        }
+        $hash = dechex(bindec($bits))."\n";
+
         $im->clear();
 
         if (isset($this->hashes[$hash])) {
